3560b529bef684e1011ed07099fe81ca19299e81
Juha-Samuli Hellen <juha-samuli.hellen@taaleri.com>
2021-04-25 16:39:06 +0300

    add: code

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+from flask import Flask, request, Response, json
 
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["POST"])
 def main():
     print("Hello, World!")
 
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+
+    request_data = request.get_json()
+
+    a = request_data["a"]
+    b = request_data["b"]
+
+    response = {"result": a + b}
+
+    return Response(json.dumps(response), status=200, mimetype='application/json')
+
+
 if __name__ == '__main__':
-    main()
+    app.run(debug=False)
